0c4dd951cf7dfe1f8320dad9e2ebd5619a954ad5
Victoria Dunham <vkurzweg@gmail.com>
2016-08-29 16:21:10 -0700

    update functions

--- js/main.js
+++ js/main.js
@@ -1,213 +1,128 @@
-$(function play(){
+// $(function(){
 
 //DATA MODEL
-var payline = [0,0,0]
-var bank = 100;
+var payline = [null, null, null];
+var bank = 20;
 var payout = null;
 var bet = null;
+var symbolCounts = {};
 
-//RANDOM NUMBER GENERATOR
-$('.buttons').on('click', function(){
-  var random1 = Math.floor((Math.random() * 10));
-  payline.splice(0,1,random1);
-  var random2 = Math.floor((Math.random() * 10));
-  payline.splice(1,1,random2);
-  var random3 = Math.floor((Math.random() * 10));
-  payline.splice(2,1,random3);
-  console.log(payline);
-})
+var symbols = [
+  {
+    name: 'Ghost',
+    image: 'images/ghost.png',
+    pays: 100
+  }, {
+    name: 'Cherry',
+    image: 'images/cherry.png',
+    pays: 90
+  }, {
+    name: 'Chart',
+    image: 'images/chart-upward-trend.png',
+    pays: 80
+  }, {
+    name: 'Pineapple',
+    image: 'images/pineapple.png',
+    pays: 70
+  }, {
+    name: 'Diamond',
+    image: 'images/diamond-with-dot.png',
+    pays: 60
+  }, {
+    name: 'Crab',
+    image: 'images/crab.png',
+    pays: 50
+  }, {
+    name: 'Sunrise',
+    image: 'images/sunrise.png',
+    pays: 40
+  }, {
+    name: 'Wave',
+    image: 'images/water-wave.png',
+    pays: 30
+  }, {
+    name: 'Balloon',
+    image: 'images/balloon.png',
+    pays: 20
+  }, {
+    name: 'Wavy',
+    image: 'images/wavy-dash.png',
+    pays: 10
+  }
+];
 
-  //PAYOUT
-$('#bet1').on('click', function(){
-  $('#payout').html('');
-  //3 Match
-  if(payline === [0,0,0]){
-    payout += 3300;
-  }
-  if(payline === [1,1,1]){
-    payout += 2000;
-  }
-  if(payline === [2,2,2]){
-    payout += 1000;
-  }
-  if(payline === [3,3,3]){
-    payout += 500;
-  }
-  if(payline === [4,4,4]){
-    payout += 250;
-  }
-  if(payline === [5,5,5]){
-    payout += 100;
-  }
-  if(payline === [6,6,6]){
-    payout += 80;
-  }
-  if(payline === [7,7,7]){
-    payout += 30;
-  }
-  if(payline === [8,8,8]){
-    payout += 15;
-  }
-  if(payline === [9,9,9]){
-    payout += 3;
-  }
-  //2 Match
-  if((payline[0] && payline[1] === 0) || (payline[0] && payline[2] === 0) || (payline[1] && payline[2] === 0)){
-    payout += 100;
-  }
-  if((payline[0] && payline[1] === 1) || (payline[0] && payline[2] === 1) || (payline[1] && payline[2] === 1)){
-    payout += 50;
-  }
-  if((payline[0] && payline[1] === 2) || (payline[0] && payline[2] === 2) || (payline[1] && payline[2] === 2)){
-    payout += 25;
-  }
-  if((payline[0] && payline[1] === 3) || (payline[0] && payline[2] === 3) || (payline[1] && payline[2] === 3)){
-    payout += 10;
-  }
-  if((payline[0] && payline[1] === 4 || 5 || 6) || (payline[0] && payline[2] === 4 || 5 || 6) || (payline[1] && payline[2] === 4 || 5 || 6)){
-    payout += 5;
-  }
-  if((payline[0] && payline[1] === 7 || 8 || 9) || (payline[0] && payline[2] === 7 || 8 || 9) || (payline[1] && payline[2] === 7 || 8 || 9)){
-    payout += 1;
-  }
-  //Single
-  if(payline[0] === 0 || payline[1] === 0 || payline[2] === 0){
-    payout += 5;
-  }
-  if(payline[0] === 1 || payline[1] === 1 || payline[2] === 1){
-    payout += 2;
-  }
-  if(payline[0] === 2 || payline[1] === 2 || payline[2] === 2){
-    payout += 1;
-  } else {
-    payout = 0;
-  }
-  console.log(payout);
+var distribution = [0,0,0,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4,5,5,5,5,5,5,6,6,6,6,6,6,6,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,9,9,9,9,9,9,9,9,9,9];
 
-  $('#payout').append(payout);
-})
+function countSymbols() {
+  symbolCounts = {};
+  payline.forEach(function(symbol) {
+    // check if key exists in counts for the current symbol
+    if (symbolCounts[symbol]) {
+      // increment count
+      symbolCounts[symbol]++;
+    } else {
+      symbolCounts[symbol] = 1;
+    }
+  });
+}
 
+function computePayout() {
 
-//VIEW
+  // loop thru the symbolCounts object
+  var keys = Object.keys(symbolCounts);
+  for (var i=0; i < keys.length; i++) {
+    if (symbolCounts[keys[i]] === 2) {
+      payout = 2 * symbols[keys[i]].pays;
+      break;
+    } else if (symbolCounts[keys[i]] === 3) {
+      payout = 10 * symbols[keys[i]].pays;
+      break;
+    } else {
+      payout = 0;
+    };
+  }
+  bank += payout;
+  console.log(bank,payout,symbolCounts);
+}
 
-var $ghost = $( '<img src="images/ghost.png">' );
-var $cherry = $( '<img src="images/cherry.png">' );
-var $chartwut = $( '<img src="images/chart-upward-trend.png">' );
-var $pineapple = $( '<img src="images/pineapple.png">' );
-var $diamond = $( '<img src="images/diamond-with-dot.png">' );
-var $crab = $( '<img src="images/crab.png">' );
-var $sunrise = $( '<img src="images/sunrise.png">' );
-var $wave = $( '<img src="images/water-wave.png">' );
-var $balloon = $( '<img src="images/balloon.png">' );
-var $wavy = $( '<img src="images/wavy-dash.png">' );
+function render() {
+  // loop thru each reel value in payline array
+  for (var i = 0; i < payline.length; i++) {
+    // update the src property of <img id="reelImage0"...
+    $('#reelImage' + i).attr('src', symbols[payline[i]].image);
+  }
+  $('#payout').html(payout);
+  $('#bank').html(bank);
+}
 
 
 
 $('.buttons').on('click', function(){
-  $('reel1').html();
-  $('reel2').html();
-  $('reel3').html();
-
-
-  switch (payline[0]){
-    case 0:
-      $('#reel1').append($ghost);
+    // generate symbols
+  for (var i = 0; i < payline.length; i++) {
+    payline[i] = distribution[Math.floor(Math.random() * distribution.length)];
+  }
+  bet = this.id;
+  // set bet amount
+  countSymbols();
+  switch(bet) {
+    case 'bet1':
+      bank -= 1;
+      computePayout();
       break;
-    case 1:
-      $('#reel1').append($cherry);
+    case 'bet2':
+      bank -= 2;
+      computePayout() * 2;
       break;
-    case 2:
-      $('#reel1').append($chartwut);
+    case 'bet3':
+      bank -= 3;
+      computePayout() * 3;
       break;
-    case 3:
-      $('#reel1').append($pineapple);
-      break;
-    case 4:
-      $('#reel1').append($diamond);
-      break;
-    case 5:
-      $('#reel1').append($crab);
-      break;
-    case 6:
-      $('#reel1').append($sunrise);
-      break;
-    case 7:
-      $('#reel1').append($wave);
-      break;
-    case 8:
-      $('#reel1').append($balloon);
-      break;
-    case 9:
-      $('#reel1').append($wavy);
-      break;
-    };
-
-  switch (payline[1]){
-    case 0:
-      $('#reel2').append($ghost);
-      break;
-    case 1:
-      $('#reel2').append($cherry);
-      break;
-    case 2:
-      $('#reel2').append($chartwut);
-      break;
-    case 3:
-      $('#reel2').append($pineapple);
-      break;
-    case 4:
-      $('#reel2').append($diamond);
-      break;
-    case 5:
-      $('#reel2').append($crab);
-      break;
-    case 6:
-      $('#reel2').append($sunrise);
-      break;
-    case 7:
-      $('#reel2').append($wave);
-      break;
-    case 8:
-      $('#reel2').append($balloon);
-      break;
-    case 9:
-      $('#reel2').append($wavy);
-      break;
-    };
-
-    switch (payline[2]){
-    case 0:
-      $('#reel3').append($ghost);
-      break;
-    case 1:
-      $('#reel3').append($cherry);
-      break;
-    case 2:
-      $('#reel3').append($chartwut);
-      break;
-    case 3:
-      $('#reel3').append($pineapple);
-      break;
-    case 4:
-      $('#reel3').append($diamond);
-      break;
-    case 5:
-      $('#reel3').append($crab);
-      break;
-    case 6:
-      $('#reel3').append($sunrise);
-      break;
-    case 7:
-      $('#reel3').append($wave);
-      break;
-    case 8:
-      $('#reel3').append($balloon);
-      break;
-    case 9:
-      $('#reel3').append($wavy);
-      break;
-    };
-
+  };
+  render();
 })
 
-});
+
+
+
+// });
---
+++ js/symbols.js
@@ -0,0 +1,43 @@
+var symbols = [
+  {
+    name: 'Ghost',
+    image: 'images/ghost.png',
+    pays: 100
+  }, {
+    name: 'Cherry',
+    image: 'images/cherry.png',
+    pays: 90
+  }, {
+    name: 'Chart',
+    image: 'images/chart-upward-trend.png',
+    pays: 80
+  }, {
+    name: 'Pineapple',
+    image: 'images/pineapple.png',
+    pays: 70
+  }, {
+    name: 'Diamond',
+    image: 'images/diamond-with-dot.png',
+    pays: 60
+  }, {
+    name: 'Crab',
+    image: 'images/crab.png',
+    pays: 50
+  }, {
+    name: 'Sunrise',
+    image: 'images/sunrise.png',
+    pays: 40
+  }, {
+    name: 'Wave',
+    image: 'images/water-wave.png',
+    pays: 30
+  }, {
+    name: 'Balloon',
+    image: 'images/balloon.png',
+    pays: 20
+  }, {
+    name: 'Wavy',
+    image: 'images/wavy-dash.png',
+    pays: 10
+  }
+];
